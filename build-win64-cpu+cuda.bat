70bab0f9101823840933dd3f41e52ef10c7b2da2
Trenton <130304830+Trentonom0r3@users.noreply.github.com>
2023-10-03 02:04:11 -0500

    Add files via upload

--- build-win64-cpu+cuda.bat
+++ build-win64-cpu+cuda.bat
@@ -10,8 +10,8 @@ call "vcvarsall.bat" amd64
 set PYTHON_INCLUDE="C:/Python311/include"
 :: Include the path to Python libs
 set PYTHON_LIB="C:/Python311/libs"
-
-nvcc -gencode arch=compute_50,code=sm_50 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_86,code=sm_86 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu src/wrapper.cpp -DNDEBUG -O3 -I "include" -I %PYTHON_INCLUDE% -I "C:/Users/tjerf/vcpkg/installed/x64-windows-static/include" -I "C:/Users/tjerf/vcpkg/installed/x64-windows/include" -o "bin/ebsynth.pyd" -Xcompiler "/openmp /fp:fast" -Xlinker "/IMPLIB:lib/ebsynth.lib" -shared "C:/Python311/libs/python311.lib" "C:/Users/tjerf/Downloads/opencv/build/x64/vc16/lib/opencv_world480.lib" -DEBSYNTH_API=__declspec(dllexport) -w || goto error
+:: -gencode arch=compute_50,code=sm_50 -gencode arch=compute_70,code=sm_70
+nvcc --std c++17 -gencode arch=compute_86,code=sm_86 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu src/wrapper.cpp -DNDEBUG -O3 -I "include" -I %PYTHON_INCLUDE% -I "C:/Users/tjerf/vcpkg/installed/x64-windows-static/include" -I "C:/Users/tjerf/vcpkg/installed/x64-windows/include" -o "bin/ebsynth.pyd" -Xcompiler "/std:c++17 /openmp /fp:fast" -Xlinker "/IMPLIB:lib/ebsynth.lib" -shared "C:/Python311/libs/python311.lib" "C:/Users/tjerf/Downloads/opencv/build/x64/vc16/lib/opencv_world480.lib" -DEBSYNTH_API=__declspec(dllexport) -w || goto error
 
 del dummy.lib;dummy.exp 2> NUL
 goto :EOF
