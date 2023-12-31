ce3139c79d8ac5fd274cd0b2e63ea6ffa9aea07c
Trenton <130304830+Trentonom0r3@users.noreply.github.com>
2023-12-31 05:59:09 -0600

    Add files via upload

--- build-linux-cpu+cuda.sh
+++ build-linux-cpu+cuda.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nvcc -arch compute_30 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu -I"include" -DNDEBUG -D__CORRECT_ISO_CPP11_MATH_H_PROTO -O6 -std=c++11 -w -Xcompiler -fopenmp -o bin/ebsynth
+nvcc -gencode arch=compute_50,code=sm_50 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_86,code=sm_86 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu -I"include" -DNDEBUG -D__CORRECT_ISO_CPP11_MATH_H_PROTO -O6 -std=c++11 -w -Xcompiler -fopenmp -o bin/ebsynth
