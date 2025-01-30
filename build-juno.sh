bdd83a8a98d7dc1a573224123bef71879568ba82
Aleks <crni.aki@gmail.com>
2025-01-30 10:55:51 +0100

    Create build-juno.sh

---
+++ build-juno.sh
@@ -0,0 +1,1 @@
+nvcc -arch compute_75 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu -I"include" -DNDEBUG -D__CORRECT_ISO_CPP11_MATH_H_PROTO -O6 -std=c++11 -w -Xcompiler -fopenmp -o bin/ebsynth
